3789d4f72aaa9e270e92f09ec3338ea2bb84f6a7
RobiDomin <104741060+RobiDomin@users.noreply.github.com>
2025-03-23 23:16:11 +0100

    Add files via upload

---
+++ Parygmatobj1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Paradygmatobj1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Trapez trapezoid = new Trapez();
+            trapezoid.ReadData();
+            trapezoid.ProcessData();
+            trapezoid.ShowResults();
+        }
+    }
+}
---
+++ Parygmatobj1/Trapez.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Paradygmatobj1
+{
+
+    public class Trapez
+    {
+        private double a, b, h, surfaceArea;
+
+        public void ReadData()
+        {
+            Console.Write("Podaj długość podstawy a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj długość podstawy b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wysokość h: ");
+            h = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void ProcessData()
+        {
+            surfaceArea = ((a + b) * h) / 2;
+        }
+
+        public void ShowResults()
+        {
+            Console.WriteLine($"Podstawa a: {a:F1}");
+            Console.WriteLine($"Podstawa b: {b:F1}");
+            Console.WriteLine($"Wysokość h: {h:F1}");
+            Console.WriteLine($"Pole trapezu: {surfaceArea:F1}");
+        }
+    }
+}
